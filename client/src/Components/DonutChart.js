f6f9c565ff6820945cf4e1d02304863fd0c37cfc
Jack Stride <jackstride@jacks-mbp.home>
2020-02-25 20:04:25 +0000

    Donut chart

--- client/src/Components/DonutChart.js
+++ client/src/Components/DonutChart.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { ascending } from 'd3';
 const axios = require("axios")
+const d3 = require("d3");
 
 export default class DonutChart extends Component {
     constructor(props) {
@@ -16,14 +18,68 @@ export default class DonutChart extends Component {
             let data = res.data.dataArray;
              data = data.filter(data => data.IMDB_Rating >= 89);
             this.setState({data: data})
+            this.chart();
         }).catch(err => console.log(err))
     }
+
+
+chart = () => {
+    
+    let height = 300;
+    let width = 300;
+    let thickness = 30;
+    let duration = 750;
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    let radius = Math.min(width,height) /2;
+
+    let svg = d3.select("#donutchart")
+    .append('svg')
+    .attr("class","pie")
+    .attr("height",height)
+    .attr("width",width);
+    
+    let g = svg.append('g')
+    .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
+
+    let arc = d3.arc()
+    .innerRadius(radius - thickness)
+    .outerRadius(radius)
+
+
+    let pie = d3.pie()
+    .value(function(d,i){
+    return d.IMDB_Rating
+    }).sort(ascending)
+
+    let path = g.selectAll('path')
+    .data(pie(this.state.data))
+    .enter()
+    .append("g")
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', (d,i) => color(i))
+    .each(function(d, i) { this._current = i; })
+    
+
+
+    
+
+
+
+}
+
+
+
+
+
+
 
 
     render() {
         return (
             <div className="partTwo">
-                <h1> Hello</h1>
+                <div id="donutchart"></div>
             </div>
         )
     }
