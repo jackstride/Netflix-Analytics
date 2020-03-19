2991ed0863160de8052a8d94bd0dfca1aa073abe
Jackstride <jackstride@outlook.com>
2020-03-19 10:00:02 +0000

    Updates

---
+++ precache-manifest.30be61cedc6dfa6f8ea7414c706d3cfc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8471fb19f9af4f27fa08ded96298f18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54832ab05f396c9f2264",
+    "url": "/static/css/main.8eb55b73.chunk.css"
+  },
+  {
+    "revision": "371988e209bb0f27994a",
+    "url": "/static/js/2.878ddbaa.chunk.js"
+  },
+  {
+    "revision": "d8080348ff2064724b36f2ca2f82999a",
+    "url": "/static/js/2.878ddbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54832ab05f396c9f2264",
+    "url": "/static/js/main.634013a7.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "f7ee365f3bd17734ca063e6b72fa5fc5",
+    "url": "/static/media/NetflixShows.f7ee365f.csv"
+  },
+  {
+    "revision": "1d50c52594fe1585fd76f76c03daf06a",
+    "url": "/static/media/StockData.1d50c525.csv"
+  },
+  {
+    "revision": "d269c914e1f022d3192b2e79bb423ef6",
+    "url": "/static/media/StreamingCompetition.d269c914.csv"
+  },
+  {
+    "revision": "5eba11050a34ebb935eb8ea6969c2836",
+    "url": "/static/media/WorldwideSubscriptions.5eba1105.csv"
+  },
+  {
+    "revision": "0e422ecde8934f0afcf624d8c1691660",
+    "url": "/static/media/analytics.0e422ecd.svg"
+  },
+  {
+    "revision": "14d8e46fbc937110d85b5fce16686d41",
+    "url": "/static/media/hoc3.14d8e46f.png"
+  },
+  {
+    "revision": "8d1eda7405ba1d586cf35edbc48e5de6",
+    "url": "/static/media/info-solid.8d1eda74.svg"
+  },
+  {
+    "revision": "7ece1bc1558c595ac9acfb88a88afb6e",
+    "url": "/static/media/netflix.7ece1bc1.png"
+  }
+]);
