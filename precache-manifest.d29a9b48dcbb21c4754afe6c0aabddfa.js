36873327695b4a2c8a012993da65982545beac7e
Jackstride <jackstride@outlook.com>
2020-03-19 11:09:50 +0000

    Updates

---
+++ precache-manifest.d29a9b48dcbb21c4754afe6c0aabddfa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb6d0c1fe42016e2c75aedc2bc9a199",
+    "url": "/Netflix-Analytics/index.html"
+  },
+  {
+    "revision": "54832ab05f396c9f2264",
+    "url": "/Netflix-Analytics/static/css/main.8eb55b73.chunk.css"
+  },
+  {
+    "revision": "371988e209bb0f27994a",
+    "url": "/Netflix-Analytics/static/js/2.878ddbaa.chunk.js"
+  },
+  {
+    "revision": "d8080348ff2064724b36f2ca2f82999a",
+    "url": "/Netflix-Analytics/static/js/2.878ddbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54832ab05f396c9f2264",
+    "url": "/Netflix-Analytics/static/js/main.634013a7.chunk.js"
+  },
+  {
+    "revision": "471f3b976de682398b2e",
+    "url": "/Netflix-Analytics/static/js/runtime-main.1ed35051.js"
+  },
+  {
+    "revision": "f7ee365f3bd17734ca063e6b72fa5fc5",
+    "url": "/Netflix-Analytics/static/media/NetflixShows.f7ee365f.csv"
+  },
+  {
+    "revision": "1d50c52594fe1585fd76f76c03daf06a",
+    "url": "/Netflix-Analytics/static/media/StockData.1d50c525.csv"
+  },
+  {
+    "revision": "d269c914e1f022d3192b2e79bb423ef6",
+    "url": "/Netflix-Analytics/static/media/StreamingCompetition.d269c914.csv"
+  },
+  {
+    "revision": "5eba11050a34ebb935eb8ea6969c2836",
+    "url": "/Netflix-Analytics/static/media/WorldwideSubscriptions.5eba1105.csv"
+  },
+  {
+    "revision": "0e422ecde8934f0afcf624d8c1691660",
+    "url": "/Netflix-Analytics/static/media/analytics.0e422ecd.svg"
+  },
+  {
+    "revision": "14d8e46fbc937110d85b5fce16686d41",
+    "url": "/Netflix-Analytics/static/media/hoc3.14d8e46f.png"
+  },
+  {
+    "revision": "8d1eda7405ba1d586cf35edbc48e5de6",
+    "url": "/Netflix-Analytics/static/media/info-solid.8d1eda74.svg"
+  },
+  {
+    "revision": "7ece1bc1558c595ac9acfb88a88afb6e",
+    "url": "/Netflix-Analytics/static/media/netflix.7ece1bc1.png"
+  }
+]);
